78e91608131e2fd59af394827f849fda5c83921b
arezey <devnull@localhost>
2013-05-22 22:19:20 +0300

    Tag version 0.1 alpha (for real this time)

--- src/common.h
+++ src/common.h
@@ -35,9 +35,9 @@
 
 #define APPNAME "LDForge"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 0
-#define VERSION_PATCH 999
-#define BUILD_ID BUILD_INTERNAL
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
+#define BUILD_ID BUILD_ALPHA
 
 // =============================================
 #if (BUILD_ID != BUILD_INTERNAL)
