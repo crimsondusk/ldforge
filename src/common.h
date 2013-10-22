613ef8575b8f67b25cb4cae591cd9f0c48bdf5b4
slatenails <arezey@gmail.com>
2013-10-22 13:47:41 +0300

    enclosed the assert replacement in braces. In release mode, the assert is replaced with nothing and there's a little bit of code in addObjectDialog which uses it as a statement in an one-line if(). With the assert disappearing in release builds this triggered a GCC warning, as the if would be left without a body.

--- src/common.h
+++ src/common.h
@@ -143,9 +143,9 @@ void assertionFailure (const char* file, int line, const char* funcname, const c
 #endif // assert
 
 #ifdef DEBUG
-# define assert(N) ((N) ? (void) 0 : assertionFailure (__FILE__, __LINE__, FUNCNAME, #N))
+# define assert(N) { ((N) ? (void) 0 : assertionFailure (__FILE__, __LINE__, FUNCNAME, #N)) }
 #else
-# define assert(N)
+# define assert(N) {}
 #endif // DEBUG
 
 // Version string identifier
