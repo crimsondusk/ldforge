c4b7ad9efc5e8ec5ffc00de8069eecfc34dac9c9
crimsondusk <crimsondusk64@gmail.com>
2013-12-19 14:28:40 +0200

    - tagged 0.2.1

--- src/common.h
+++ src/common.h
@@ -38,7 +38,7 @@
 #define APPNAME "LDForge"
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 #define BUILD_ID BUILD_ALPHA
 
 // =============================================
